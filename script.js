25bf899bca782df25ad321b83a8dd02ab82450a2
Nguyen Nghia <nghianguyen150006@gmail.com>
2026-05-24 16:51:36 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+const websites = [
+  { name: "Buyer", url: "./Buyer/index.html" },
+  { name: "iTube", url: "./iTube/index.html" },
+  { name: "MusicListener", url: "./MusicListener/index.html" },
+  { name: "Stream", url: "./Stream/index.html" },
+  { name: "YourTime", url: "./YourTime/index.html" }
+];
